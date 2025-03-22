7cc002bf4ed2bea85dd15e73a51b7c31cc5fc5cb
leo <leonardo.bodan@emanuel.ro>
2025-03-23 00:31:09 +0200

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Carte carte1 = new Carte(150);
+        Carte carte2 = new Carte(1250);
+        Carte carte3 = new Carte(200);
+
+        System.out.println("Cartea 1 si Cartea 2 sunt identice: " + carte1.esteIdentica(carte2));
+        System.out.println("Cartea 1 si Cartea 3 sunt identice: " + carte1.esteIdentica(carte3));
+    }
+}
---
+++ Main4.java
@@ -0,0 +1,10 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Piramida p1 = new Piramida(4);
+        Piramida p2 = new Piramida(5);
+
+        p1.afiseaza();
+        System.out.println();
+        p2.afiseaza();
+    }
+}
